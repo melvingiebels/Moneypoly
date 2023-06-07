16ae6a8ef84580da285cbfe0256499f5719ed03c
TimdeLaater <tdelaater@gmail.com>
2023-06-07 23:27:10 +0200

    Working stock display within the inventory

--- Moneypoly/Assets/Scripts/PlayerInventory.cs
+++ Moneypoly/Assets/Scripts/PlayerInventory.cs
@@ -9,36 +9,53 @@ public class PlayerInventory : MonoBehaviour
     public TextMeshProUGUI bugetText;
     public TextMeshProUGUI netWorthText;
     public TextMeshProUGUI stockOverviewText;
-
     private List<Stock> wallet = new List<Stock>();
     private float netWorth;
 
     void Start()
     {
+       
         buget = 2000;
         bugetText.text = buget.ToString();
-        netWorthText.text = netWorth.ToString();
+        netWorthText.text = "€ " + netWorth.ToString();
+        //create stock
+        Stock stock1 = new Stock();
+        stock1.currentPrice = 100;
+        stock1.name = "stock1";
+        BuyStock(stock1);
+        PrintStock();
+        CalculateWallet();
 
     }
 
     void Update()
     {
-
+        
+        bugetText.text = buget.ToString();
+        netWorthText.text = "€ "+ netWorth.ToString();
+        
     }
 
     public void CalculateWallet()
     {
-        netWorth = 0;
+        
         foreach(Stock stock in wallet)
         {
-            netWorth =+ stock.currentPrice;
+            netWorth += stock.currentPrice;
+        }
+    }
+    public void PrintStock()
+    {
+        foreach(Stock stock in wallet)
+        {
+            stockOverviewText.text = stockOverviewText.text + stock.name + " € " + stock.currentPrice + "\n";
         }
     }
 
 
     public void BuyStock(Stock stock)
     {
-            buget =-stock.currentPrice;
+            buget -= stock.currentPrice;
             wallet.Add(stock);
         
     }
@@ -47,5 +64,6 @@ public class PlayerInventory : MonoBehaviour
 public class Stock
 {
     public float currentPrice;
+    public string name;
 
 }
