851fa1ecce7c0562a62eb778abfe61f27a9efca3
TimdeLaater <tdelaater@gmail.com>
2023-06-09 17:03:55 +0200

    Fineshed the newsflash scene with Scenecontroller

---
+++ Moneypoly/Assets/Scripts/SceneController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public static SceneController instance;
+
+    private void Awake()
+    {
+        // Ensure only one instance of the SceneController exists
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
--- Moneypoly/Assets/Scripts/VideoPlayerController.cs
+++ Moneypoly/Assets/Scripts/VideoPlayerController.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Video;
@@ -7,7 +8,8 @@ public class VideoPlayerController : MonoBehaviour
     public VideoPlayer videoPlayer;
     public RawImage rawImage;
     public Image imageToShow;
-
+    public TextMeshProUGUI newsFlashText;
+    public Button continueButton;
     void Start()
     {
         videoPlayer.audioOutputMode = VideoAudioOutputMode.None;
@@ -18,11 +20,28 @@ public class VideoPlayerController : MonoBehaviour
         FindObjectOfType<AudioManager>().Play("Nostechno");
 
         videoPlayer.loopPointReached += OnVideoEnd;
+
+        //Click for next scene 
+        
+        continueButton.onClick.AddListener(SwitchScene);
+
+    }
+    public void SetNewsText(string text)
+    {
+        newsFlashText.text = text;
     }
 
     void OnVideoEnd(VideoPlayer vp)
     {
         rawImage.enabled = false;
         imageToShow.enabled = true;
+        newsFlashText.enabled = true;
+    }
+
+    public void SwitchScene()
+    {
+        //load the next scene 
+        SceneController.instance.LoadScene("StockScene");
+
     }
 }
