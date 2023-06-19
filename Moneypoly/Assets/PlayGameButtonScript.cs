526796b67d765db7e0f4199a3ac4958a7d07834d
MauricedeRidder <mauricederidder@outlook.com>
2023-06-19 11:49:39 +0200

    piuuszah

--- Moneypoly/Assets/PlayGameButtonScript.cs
+++ Moneypoly/Assets/PlayGameButtonScript.cs
@@ -8,7 +8,7 @@ using UnityEngine.SceneManagement;
 public class PlayGameButtonScript : MonoBehaviour
 {
     public List<PlayerSelectTab> playerSelectedScreens;
-    public GameObject inventoryScreenPrefab;
+    public PlayerSelectionManager playerSelectionManager;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,49 +22,24 @@ public class PlayGameButtonScript : MonoBehaviour
     }
     public void StartGameButtonClick()
     {
-        SceneManager.UnloadSceneAsync("PlayGameScene");
-        SceneManager.LoadScene("MauriceScene");
-
-        StartCoroutine(StartGame());
+        StartCoroutine(LoadSceneAndSetPlayerSelections());
     }
-    public IEnumerator StartGame()
+
+    private IEnumerator LoadSceneAndSetPlayerSelections()
     {
-        
-        string sceneName = "MauriceScene";
-        Scene loadedScene = SceneManager.GetSceneByName(sceneName);
+        // Load the scene asynchronously
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("MauriceScene", LoadSceneMode.Additive);
 
-        if (!loadedScene.isLoaded)
+        // Wait until the scene is fully loaded
+        while (!asyncLoad.isDone)
         {
-            AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
-
-            while (!asyncLoad.isDone)
-            {
-                yield return null;
-            }
-
-            loadedScene = SceneManager.GetSceneByName(sceneName);
+            yield return null;
+            // Once the scene is loaded, set player selections
         }
 
-        if (loadedScene.isLoaded)
-        {
-            BoardController boardController = loadedScene.GetRootGameObjects().FirstOrDefault(go => go.name == "BoardController")?.GetComponent<BoardController>();
-
-            float yChord = -164f;
-            foreach (PlayerSelectTab playerSelectTab in playerSelectedScreens)
-            {
-                GameObject player = new GameObject("Player" + playerSelectTab.PlayerNameText);
-                player.transform.SetParent(boardController.transform); // Attach to the root object of the scene
-                PlayerController playerController = player.AddComponent<PlayerController>();
-                playerController.playerInventory = new PlayerInventory();
-                GameObject inventoryScreen = Instantiate(inventoryScreenPrefab, player.transform);
-
-                playerController.Initialize(playerSelectTab.PlayerImage.sprite, yChord++); // Pass player-specific parameters
-                boardController.players.Add(playerController);
-            }
-        }
-       
+        // Once the scene is loaded, set player selections
+        playerSelectionManager.SetPlayerSelections(playerSelectedScreens);
     }
-
     public void BackToMainMenu()
     {
         SceneManager.UnloadSceneAsync("PlayGameScene");
